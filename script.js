2314b9ebb134bad22f3bbe4814c44a1c763cc956
CHINOGA <43039717+CHINOGA@users.noreply.github.com>
2023-10-18 07:17:32 +0000

    responsive and adaptive feature

--- script.js
+++ script.js
@@ -4,3 +4,27 @@ function myFunction() {
 }
 
 document.addEventListener("DOMContentLoaded", myFunction);
+// script.js
+// Self-invoking function to prevent global scope pollution
+(function(){
+  // Use 'use strict' to make code more robust and manageable
+  'use strict';
+
+  /**
+   * This is the main function where most logic for the website resides
+   */
+  const myFunction = () => {
+    // Add any JavaScript code here
+    // Example:
+    // Set up an event listener for a hypothetical button which does something when clicked
+    const someButton = document.getElementById('someButton');
+    if(someButton) {
+      someButton.addEventListener('click', (event) => {
+        event.preventDefault();
+        console.log("Button is clicked!");
+      });
+    }
+  }
+  // Wait until document has fully loaded, then run myFunction
+  window.addEventListener("DOMContentLoaded", myFunction);
+})();
